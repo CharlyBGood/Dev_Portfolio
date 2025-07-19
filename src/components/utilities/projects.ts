469481520944e8e2d2401f1986199b80f38ecd07
charlybgood__x <bonavita.carlos@gmail.com>
2025-07-19 16:30:12 -0300

    update projects list

--- src/components/utilities/projects.ts
+++ src/components/utilities/projects.ts
@@ -14,9 +14,9 @@ const projects: Project[] = [
       "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1750828268/horsey_pegjou.svg",
     title: "NoStateNode",
     description:
-      "WebApp en modo -Lista de Recursos- para compartir entre contactos. Autenticación con Google y almacenamiento de recursos en Firebase/Firestore.",
+      "Aplicación web para compartir recursos entre contactos. Autenticación de usuarios y almacenamiento en la nube.",
     url: "https://nostatenode.site/",
-    tags: ["React", "CSS", "Firebase/Firestore", "Google Auth/OAuth", "Vercel"],
+    tags: ["React", "CSS", "Firebase/Firestore", "Google Auth/OAuth"],
   },
   {
     id: 2,
@@ -24,16 +24,13 @@ const projects: Project[] = [
       "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1750824112/robot_dnipig.png",
     title: "Chatbot Widget Customizable",
     description:
-      "Widget de chatbot personalizable, configurado para conectarse directamente con backend optimizado para integración modelos de IA.",
+      "Widget de chatbot personalizable, configurado para conectarse directamente con endpoint de backend para gestión de LLMs.",
     url: "https://chatbotfrontend-tau.vercel.app/",
     tags: [
-      "API",
-      "LLMs Integration/Embeddings",
-      "Google Cloud/Cloud Run",
-      "NodeJS/Express",
+      "Chatbot Widget",
+      "LLMs Integration",
       "React/Vite",
       "TailwindCSS",
-      "Vercel",
     ],
   },
   {
@@ -42,9 +39,16 @@ const projects: Project[] = [
       "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1750881465/902fae94-b60e-4761-916b-780ffd735b3a.png",
     title: "Asegurando Online",
     description:
-      "Sitio web personalizado para Maschio y Asociados, enfocado en llamada a la acción (CTA) con Integración de chatbot para atención al cliente y generación de leads.",
+      "Landing Page para Maschio y Asociados con Integración de chatbot para atención al cliente y generación de leads.",
     url: "https://asegurando.online/",
-    tags: ["React", "TailwindCSS", "Cloudinary", "Chatbot Integration"],
+    tags: [
+      "React",
+      "TailwindCSS",
+      "Chatbot",
+      "LLMs Integration",
+      "NodeJS/Express",
+      "Google Cloud/Cloud Run",
+    ],
   },
   {
     id: 4,
@@ -52,16 +56,9 @@ const projects: Project[] = [
       "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1751168366/5680e11c-0f48-44f0-a4a2-d531f918958b.png",
     title: "Planificador Financiero",
     description:
-      "Aplicación web para el seguimiento de gastos personales, con autenticación de usuario y almacenamiento en la nube.",
+      "Aplicación web para gestión financiera, con autenticación de usuario y almacenamiento en la nube.",
     url: "https://expence-tracker-theta.vercel.app/",
-    tags: [
-      "React",
-      "TailwindCSS",
-      "Firebase/Firestore",
-      "Vercel",
-      "Google Auth",
-      "Supabase",
-    ],
+    tags: ["React/Vite", "TailwindCSS", "Google Auth", "Supabase"],
   },
   {
     id: 5,
@@ -71,7 +68,7 @@ const projects: Project[] = [
     description:
       "Herramienta de cotización de productos de seguros, con carrito de compras.",
     url: "https://stunning-toffee-a5e308.netlify.app/",
-    tags: ["React", "TailwindCSS", "Bolt.new", "Netlify"],
+    tags: ["React", "TailwindCSS", "TypeScript"],
   },
   {
     id: 6,
@@ -79,9 +76,9 @@ const projects: Project[] = [
       "https://res.cloudinary.com/dr8pwzxzn/image/upload/v1752506646/MiliFinalFinalFinalFinal_pkdza3.svg",
     title: "Mily",
     description:
-      "Aplicación web para registro de comidas y seguimiento alimenticio, con autenticación de usuario y almacenamiento en la nube.",
+      "Aplicación para registro y seguimiento alimenticio, con autenticación de usuario y almacenamiento en la nube.",
     url: "https://mily-app-lyart.vercel.app/",
-    tags: ["NextJS", "TailwindCSS", "Vercel", "Supabase", "Google Auth"],
+    tags: ["NextJS", "TailwindCSS", "Supabase", "Google Auth"],
   },
 ];
 
